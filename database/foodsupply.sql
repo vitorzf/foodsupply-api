539a8e8545187635d15448b96508dea48eba4a8a
vitorzf <igovitorbr@gmail.com>
2022-05-15 15:09:20 -0300

    update do banco de dados

--- database/foodsupply.sql
+++ database/foodsupply.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-Maio-2022 às 02:55
+-- Generation Time: 15-Maio-2022 às 20:08
 -- Versão do servidor: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -25,13 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `carrinho`
+--
+
+CREATE TABLE `carrinho` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `categorias`
 --
 
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `nome` int(11) NOT NULL
+  `nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nome`) VALUES
+(1, 'Carnes'),
+(2, 'Cerveja');
 
 -- --------------------------------------------------------
 
@@ -46,10 +67,20 @@ CREATE TABLE `endereco` (
   `cidade` varchar(100) NOT NULL,
   `bairro` varchar(255) NOT NULL,
   `endereco` varchar(255) NOT NULL,
-  `rua` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `principal` tinyint(1) NOT NULL DEFAULT '0'
+  `principal` tinyint(1) NOT NULL DEFAULT '0',
+  `ativo` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`id`, `cep`, `uf`, `cidade`, `bairro`, `endereco`, `numero`, `usuario_id`, `principal`, `ativo`) VALUES
+(1, '17511801', 'SP', 'Marília', 'Jardim Lavínia', 'Amélia Volta Laplechade', 57, 9, 0, 1),
+(2, '17511801', 'SP', 'Teresina', 'Direceu Arcoverde II', 'Quadra 333', 1, 9, 0, 1),
+(3, '17511801', 'SP', 'Teresina', 'Direceu Arcoverde II', 'Quadra 333', 3, 9, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -59,13 +90,25 @@ CREATE TABLE `endereco` (
 
 CREATE TABLE `produto` (
   `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `descricao` text NOT NULL,
+  `fotos` text,
+  `estoque` int(11) NOT NULL DEFAULT '0',
   `preco` decimal(15,2) NOT NULL,
   `categoria_id` int(11) DEFAULT NULL,
-  `medida_id` int(11) DEFAULT NULL
+  `medida_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `data_hora_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `sku`, `titulo`, `descricao`, `fotos`, `estoque`, `preco`, `categoria_id`, `medida_id`, `usuario_id`, `data_hora_cadastro`, `ativo`) VALUES
+(3, 'ProdTeste1', 'Produto de teste 1', 'Este é o teste de descrição do produto, pode dar tudo errado ou tudo certo, vai la saber', '[{\"url\":\"https://talcha.vteximg.com.br/arquivos/ids/170030-1000-1000/teste.jpg?v=637685222639430000\",\"ordem\":0},{\"url\":\"https://static3.tcdn.com.br/img/img_prod/99941/produto_teste_auaha_24522_1_f816ad73890b2db46e6e460c44ae5d22.png\",\"ordem\":1}]', 100, '140.50', 1, 2, 9, '2022-05-13 05:18:14', 1);
 
 -- --------------------------------------------------------
 
@@ -75,8 +118,20 @@ CREATE TABLE `produto` (
 
 CREATE TABLE `unidade_medida` (
   `id` int(11) NOT NULL,
+  `sigla` varchar(5) NOT NULL,
   `nome` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `unidade_medida`
+--
+
+INSERT INTO `unidade_medida` (`id`, `sigla`, `nome`) VALUES
+(1, 'KG', 'Quilo Gramas'),
+(2, 'G', 'Gramas'),
+(3, 'ML', 'Mililitros'),
+(4, 'L', 'Litros'),
+(5, 'UN', 'Unidade');
 
 -- --------------------------------------------------------
 
@@ -88,20 +143,59 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `usuario` varchar(255) NOT NULL,
+  `nome_vendedor` varchar(255) DEFAULT NULL,
   `senha` varchar(32) NOT NULL,
-  `foto` text
+  `foto` text,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `email`, `usuario`, `senha`, `foto`) VALUES
-(9, 'igovitorbr@gmail.com', 'vitorzf', 'a087483b4d2c1ce939de26df9eee360a', NULL);
+INSERT INTO `usuario` (`id`, `email`, `usuario`, `nome_vendedor`, `senha`, `foto`, `ativo`) VALUES
+(9, 'igovitorbr@gmail.com', 'vitorzf', 'Vitor Store', '202cb962ac59075b964b07152d234b70', NULL, 1),
+(10, 'monychan@gmail.com', 'monygasai', NULL, 'aa1bf4646de67fd9086cf6c79007026c', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `endereco_id` int(11) NOT NULL,
+  `valor_total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda_produto`
+--
+
+CREATE TABLE `venda_produto` (
+  `id` int(11) NOT NULL,
+  `venda_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valor_unitario` decimal(10,2) NOT NULL,
+  `valor_total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produto_id` (`produto_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indexes for table `categorias`
@@ -120,6 +214,7 @@ ALTER TABLE `endereco`
 -- Indexes for table `produto`
 --
 ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `categoria_id` (`categoria_id`),
   ADD KEY `medida_id` (`medida_id`);
 
@@ -136,36 +231,83 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `endereco_id` (`endereco_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indexes for table `venda_produto`
+--
+ALTER TABLE `venda_produto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produto_id` (`produto_id`),
+  ADD KEY `venda_id` (`venda_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `unidade_medida`
 --
 ALTER TABLE `unidade_medida`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `venda_produto`
+--
+ALTER TABLE `venda_produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Limitadores para a tabela `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD CONSTRAINT `carrinho_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `carrinho_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `endereco`
@@ -179,6 +321,20 @@ ALTER TABLE `endereco`
 ALTER TABLE `produto`
   ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `produto_ibfk_2` FOREIGN KEY (`medida_id`) REFERENCES `unidade_medida` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venda_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `venda_produto`
+--
+ALTER TABLE `venda_produto`
+  ADD CONSTRAINT `venda_produto_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venda_produto_ibfk_2` FOREIGN KEY (`venda_id`) REFERENCES `venda` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
