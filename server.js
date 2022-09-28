51130c52a1b152f2a88be9028830ce9a8b200ea7
vitorzf2 <igovitorbr@gmail.com>
2022-09-28 01:00:18 -0300

    a

--- server.js
+++ server.js
@@ -20,16 +20,16 @@ app.use("/", vendedor);
 app.use("/", pedidos);
 app.use("/", vendas);
  
-app.listen(porta, ()=>{
-
-    console.log(`Servidor iniciado na porta ${porta}`)
-
-});
-
 process.on('uncaughtException', (error, origin) => {
     if (error?.code === 'ECONNRESET') return;
     console.error('UNCAUGHT EXCEPTION');
     console.error(error);
     console.error(origin);
     process.exit(1);
-  });
+});
+
+app.listen(porta, ()=>{
+
+    console.log(`Servidor iniciado na porta ${porta}`)
+
+});
