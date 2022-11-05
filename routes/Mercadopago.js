40b3d2ebfced6c6c77c258d09068c10b3f1212be
vitorzf <igovitorbr@gmail.com>
2022-11-05 17:20:34 -0300

    alterada requisicao

--- routes/Mercadopago.js
+++ routes/Mercadopago.js
@@ -3,9 +3,9 @@ const controller = require("../app/controllers/mercadopago.controller");
 
 const router = express.Router();
 
-router.post("/mercadopago", controller.callbackMP);
-router.post("/mercadopago/pending", controller.callbackMP);
-router.post("/mercadopago/success", controller.callbackMP);
-router.post("/mercadopago/failure", controller.callbackMP);
+router.get("/mercadopago", controller.callbackMP);
+router.get("/mercadopago/pending", controller.callbackMP);
+router.get("/mercadopago/success", controller.callbackMP);
+router.get("/mercadopago/failure", controller.callbackMP);
 
 module.exports = router;
